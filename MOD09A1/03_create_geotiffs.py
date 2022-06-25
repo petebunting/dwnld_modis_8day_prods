fd9ca90479aac34405771f73c7359b8fea43712f
Pete Bunting <petebunting@mac.com>
2022-06-25 19:16:04 +0100

    Add script to create geotiffs.

---
+++ MOD09A1/03_create_geotiffs.py
@@ -0,0 +1,26 @@
+import glob
+import os
+
+def create_modis_hdf_to_gtiff(input_file, output_img):
+    import rioxarray
+    modis_obj = rioxarray.open_rasterio(input_file, masked=True)
+    modis_obj = modis_obj.isel(band=0)
+    # Geotiff outputted is not tiled or compressed and pyramids not built.
+    # Would benefit from running through gdal_translate and then building datasets
+    # stats and pyramids. Also, would be help to reorder bands.
+    modis_obj.rio.to_raster(raster_path=output_img)
+
+# Output directory where data will be downloaded to.
+out_dir = "/Users/pete/Temp/modis_processing/gtiffs"
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+# Get Modis files
+modis_files = glob.glob("/Users/pete/Temp/modis_processing/downloads/*.hdf")
+# Loop through and create geotiffs
+for modis_file in modis_files:
+    print(modis_file)
+    file_name = os.path.split(modis_file)[0].replace(".hdf", "").replace(".", "_").lower()
+    out_file = os.path.join(out_dir, "{}.tif".format(file_name))
+    create_modis_hdf_to_gtiff(modis_file, out_file)
+
